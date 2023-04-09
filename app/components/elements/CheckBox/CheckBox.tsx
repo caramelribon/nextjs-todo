7dfddf31e7d375997a0aaaa771211c3f072d8de8
caramelribon <caramelribon137@gmail.com>
2023-04-09 13:24:13 +0900

    [add] CheckBoxのコンポーネントの作成

---
+++ app/components/elements/CheckBox/CheckBox.tsx
@@ -0,0 +1,20 @@
+import styles from "./CheckBox.module.scss";
+import classNames from "classnames";
+import { CheckBoxProps } from "./CheckBox.types";
+
+const CheckBox = (props: CheckBoxProps) => {
+  const { className, isChecked = false, ...rest } = props;
+  return (
+    <label className={styles.checkbox}>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        className={classNames(className, styles.checkbox__input)}
+        {...rest}
+      />
+      <span className={styles.checkbox__span}></span>
+    </label>
+  );
+};
+
+export default CheckBox;
---
+++ app/components/elements/CheckBox/CheckBox.types.ts
@@ -0,0 +1,5 @@
+// Checkboxの型定義
+export type CheckBoxProps = JSX.IntrinsicElements["input"] & {
+    isChecked: boolean;
+};
+
